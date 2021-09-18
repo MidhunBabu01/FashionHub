a1606aa026832f1c666a928e864bbf9ea64d6dd2
Midhun babu <77397561+MidhunBabu01@users.noreply.github.com>
2021-09-18 13:23:00 +0530

    update models.py

--- fashion_hub_project/Cart/models.py
+++ fashion_hub_project/Cart/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.template.defaultfilters import default
+from django_countries.widgets import CountrySelectWidget
 from fashion_hub_app.models import Products
 from django.conf import settings
 from django_countries.fields import CountryField
@@ -28,8 +30,29 @@ class Items(models.Model):
 
 
 
-
-
-
-
-
+class Orderz(models.Model):
+    cartt = models.ManyToManyField(Items)
+    name = models.CharField(max_length=25,blank=False)
+    email = models.EmailField(blank=True)
+    mobile = models.CharField(blank=False,max_length=10 )
+    address = models.CharField(max_length=50, blank=False)
+    city = models.CharField(max_length=15, blank=False)
+    STATE_CHOICES = (
+        ('Kerala','Kerala'),
+        ('Tamilnaadu','Tamilnaadu'),
+        ('Telangana','telangana')
+    )
+    ORDER_CHOICES = (
+        ('order recieved','order recieved'),
+        ('order processing','order processing'),
+        ('on the way','on the way'),
+        ('order cancelled','order cancelled')
+    )
+    state = models.CharField(max_length=25,choices=STATE_CHOICES, default='1')
+    pin = models.IntegerField()
+    country = CountryField()    
+    # total = models.PositiveIntegerField()
+    order_status = models.CharField(max_length=25,choices=ORDER_CHOICES)
+    def __str__(self):
+        return str(self.name)
+    
